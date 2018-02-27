eadb29525b2860b37605eb5b2a88fb62e4892124
littlelinghome <littlelinehome@gmail.com>
2018-02-27 17:12:58 +0800

    优化猜年龄游戏，允许用户最多猜3次，猜了3次后，再询问是否还想再玩，如果用户选y，则再允许猜3次，以此往复。

--- 猜年龄游戏/3次之后再循环猜.py
+++ 猜年龄游戏/3次之后再循环猜.py
@@ -16,3 +16,19 @@ while count < 3:
         if choice == 'y' or choice == 'Y':
             count = 0
 
+###############################################################
+i = 0
+age =23
+client = 'y'
+while client == 'y':
+    while i < 3:
+        _age = int(input("Age:"))
+        if age == _age:
+            print ("you are right")
+            break
+        else:
+            print ("again")
+        i += 1
+    client = input("Client(y):")
+    if client == 'y' or client == 'Y':
+        i = 0
